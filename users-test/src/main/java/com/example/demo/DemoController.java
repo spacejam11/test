c1c73f30d1816a9973537eabc5951f4957060740
t-uehra <tadashi.uehara@temperance-st.biz>
2021-06-23 20:57:40 +0900

    tekito

--- users-test/src/main/java/com/example/demo/DemoController.java
+++ users-test/src/main/java/com/example/demo/DemoController.java
@@ -1,39 +1,27 @@
 package com.example.demo;
- 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.support.SessionStatus;
 
-
-import java.util.ArrayList;
-import java.util.List;
- 
 @Controller
 public class DemoController {
 
-    private DemoService demoService;
-    
-	public DemoController(DemoService demoService) {
-		this.demoService = demoService;
-	}
-	
+  private DemoService demoService;
 
-    @RequestMapping("/")
-    public String index(Model model){
-    	
-        //ユーザーデータリストを取得
-        List<UserData> users = demoService.getUser();
+  public DemoController(DemoService demoService) {
+    this.demoService = demoService;
+  }
 
-        model.addAttribute("user", users);
-        return "index";
-    }
- }
+
+  @RequestMapping("/")
+  public String index(Model model) {
+
+    // ユーザーデータリストを取得
+    List<UserData> users = demoService.getUser();
+
+    model.addAttribute("user", users);
+    return "index";
+  }
+}
--- users-test/src/main/java/com/example/demo/DemoService.java
+++ users-test/src/main/java/com/example/demo/DemoService.java
@@ -1,26 +1,20 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.BindingResult;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 @Service
 public class DemoService {
 
-//	@Autowired
-    private UserDataDao userDataDao;
- 
-	public DemoService(UserDataDao userDataDao) {
-		this.userDataDao = userDataDao;
-	}
-    
-    
-    public List<UserData> getUser(){
-    	
-    	return userDataDao.findAll();
-    }    
+  // @Autowired
+  private UserDataDao userDataDao;
+
+  public DemoService(UserDataDao userDataDao) {
+    this.userDataDao = userDataDao;
+  }
+
+
+  public void getUser() {
+
+    // return userDataDao.findAll();
+  }
 }
